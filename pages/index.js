d58881f9703401b56c9bda095f8702ef425d44ea
Rodrigo Frederico <167909275+RFRED19@users.noreply.github.com>
2025-03-27 16:33:38 +0000

    modificado

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <h1>"Eu te amoooo minha Ana, se voce me ama da uma risadinha!😍"</h1>}
-        <h2>{"A menina mais linda desse mundo é minha noiva!!!</h2>"}</h2>
+        {<h2>A menina mais linda desse mundo é minha noiva!!!</h2>}
 
 
 export default Home;
