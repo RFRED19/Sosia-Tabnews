d6f6260ebba68db52f330e05eee638929450d1af
Rodrigo Frederico <167909275+RFRED19@users.noreply.github.com>
2025-03-27 16:01:34 +0000

    adiconar frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Eu te amoooo minha Ana, se voce me ama da uma risadinha!😍</h1>
+    return <h1>Eu te amoooo minha Ana, se voce me ama da uma risadinha!😍
+    </h1>
 }
+<p>A menina mais linda desse mundo é minha noiva</p>
 
 export default Home;
