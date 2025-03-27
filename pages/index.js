de8122889e8844a7eab1b8f14a50141e3aaeed00
Rodrigo Frederico <167909275+RFRED19@users.noreply.github.com>
2025-03-27 16:43:11 +0000

    adicionado frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 function Home(){
-    return <h1>"Eu te amoooo minha Ana, se voce me ama da uma risadinha!😍"</h1>}
+    return (
+        <div>
+    <h1>
+        "Eu te amoooo minha Ana, se voce me ama da uma risadinha!😍"
+        </h1>
                {
-    return <h2>A menina mais linda desse mundo é minha noiva!!!</h2>}
-
+    <h2>
+        A menina mais linda desse mundo é minha noiva!!!
+        </h2>}
+    </div>
+    );
+}
 
 export default Home;
